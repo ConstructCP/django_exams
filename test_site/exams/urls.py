700d8be40406ff973cea6647beb959daed1275b8
Andrei Hubarevich <andrgch@gmail.com>
2021-10-07 19:39:03 +0300

    Exam and exam result views with tmplates

--- test_site/exams/urls.py
+++ test_site/exams/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 
+app_name = 'exams'
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('<exam_name>/', views.exam, name='exam'),
+    path('<exam_name>/results', views.exam_result, name='exam_results')
 ]
--- test_site/exams/views.py
+++ test_site/exams/views.py
@@ -1,6 +1,50 @@
+from django.http import Http404
 from django.shortcuts import render
-from django.http import HttpResponse
 
-# Create your views here.
+
+exams = ('Exam1', 'Exam2')  # stub variable
+
+
 def index(request):
-    return HttpResponse('Welcome to exams index')
+    exam_list = exams
+    context = {
+        'exam_list': exam_list
+    }
+    return render(request, 'exams/index.html', context)
+
+
+def exam(request, exam_name):
+    if exam_name not in exams:
+        raise Http404(f'No exam with name "{exam_name}" found.')
+    question_data = questions_stub()
+    return render(request, 'exams/exam.html', {'question_data': question_data, 'exam_name': exam_name})
+
+
+def exam_result(request, exam_name):
+    if exam_name not in exams:
+        raise Http404(f'No exam with name "{exam_name}" found.')
+    question_data = questions_stub()
+    correct_answers = 0
+    for question in question_data:
+        question_text = question['question_text']
+        given_answer = request.POST[question_text]
+        if int(given_answer) - 1 == question['correct_answer']:
+            correct_answers += 1
+    score = correct_answers / len(question_data)
+    score_percent = int(score * 100)
+    return render(request, 'exams/exam_results.html',
+                  {'score': score_percent, 'exam_name': exam_name})
+
+
+def questions_stub():
+    questions = ['question1', 'question2', 'question3']
+    variants = ['variant1', 'variant2']
+    question_data = []
+    for question in questions:
+        data = {
+            'question_text': question,
+            'variants': variants,
+            'correct_answer': 0
+        }
+        question_data.append(data)
+    return question_data
